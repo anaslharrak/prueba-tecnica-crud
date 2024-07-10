cab338bd1bad6938871536d5331ae6d7fc0b4209
Anas Lharrak <146816560+anaslharrak@users.noreply.github.com>
2024-07-10 13:56:17 +0200

    feat(backend): :construction: added some dependencies and created the userController (WIP:  do the endpoints)
    added the mongoose dependencie and fixed some errors with typescript, also I've added a userController and created the endpoints, but at the moment they are empty

---
+++ backend/controllers/userController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+
+class userController {
+
+    // GET /api/users
+    async findAll(req: Request, res: Response) {
+        
+    }
+    
+    // GET /api/user
+    async findOne(req: Request, res: Response) {
+       
+    }
+    
+    // POST /api/user
+    async create(req: Request, res: Response) {
+       
+    }
+    
+    // PUT /api/user
+    async update(req: Request, res: Response) {
+       
+    }
+    
+    // DELETE /api/user
+    async delete(req: Request, res: Response) {
+       
+    }
+}
+
+
+export default new userController();
--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,20 @@
-console.log('Hello, world!')
+import express from 'express'
+import userController from './controllers/userController'
+
+const app = express();
+
+app.get(`/api/users`, userController.findAll);
+
+app.get(`/api/user`, userController.findOne);
+
+app.post(`/api/user`, userController.create);
+
+app.put(`/api/user`, userController.update);
+
+app.delete(`/api/user`, userController.delete);
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+})
