75c177a00a2c2aee0f5ff62700a18cb987d32826
Anas Lharrak <146816560+anaslharrak@users.noreply.github.com>
2024-07-11 21:58:26 +0200

    feat(frontend): :sparkles: added the fetch to the find one user endpoint
    WIP: style the page

--- frontend/src/pages/UserPage/UserPage.tsx
+++ frontend/src/pages/UserPage/UserPage.tsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
+import { API_URL } from "../../constants";
+import { toast } from "sonner";
+import type { User } from "../../types";
+
 const UserPage = () => {
+
+    const [user, setUser] = useState<User>();
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`http://${API_URL}/api/user/${id}`)
+            .then(response => response.json())
+            .then(data => setUser(data.user))
+            .catch(error => toast.error('Error fetching user: ' + error.message || 'Unknown error'))
+    });
+
     return (
-        <div>
-            <h1>UserPage</h1>
-            <p>¡Welcome!</p>
-        </div>
-    )
+        user === undefined
+            ? <h1>Loading...</h1>
+            : user === null
+                ? <h1>User not found</h1>
+                : <div>
+                    <h1>{user?.name}</h1>
+                    <h1>{user?.surename}</h1>
+                    <h1>{`${user?.address.city}, ${user?.address.country}`}</h1>
+                    {/* <h1>{user?.creationDate.toLocaleDateString()}</h1> */}
+                  </div>
+    );
 }
 
 export default UserPage
